ae2732ee2a2741177691c378c250a5f6adb41805
Benjamin Camacho <jahbenjah@hotmail.com>
2021-12-28 23:04:51 -0600

    Move Test Class to a Separate File

--- tests/Singleton.Tests/TestSingleton2.cs
+++ tests/Singleton.Tests/TestSingleton2.cs
@@ -31,24 +31,3 @@ public class TestSingleton2
         Assert.False(hasPublicConstructor);
     }
 }
-
-
-public class TestUserDatabase
-{
-    [Fact]
-    public async void TestsWriteUser()
-    {
-        User user = new User() { Id = 1, Name = "Benjamin", IsActive = true };
-        _ = await UserDatabase.GetInstance().WriteUser(user);
-        Assert.True(System.IO.File.Exists("user.json"));
-    }
-
-    [Fact]
-    public async void TestReadUser()
-    {
-        User user = new User() { Id = 1, Name = "Juan", IsActive = true };
-        await UserDatabase.GetInstance().WriteUser(user);
-        var readUser = await UserDatabase.GetInstance().ReadUser("");
-        Assert.Equal("Juan", readUser.Name);
-    }
-}
---
+++ tests/Singleton.Tests/TestUserDatabase.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Singleton.Tests;
+
+public class TestUserDatabase
+{
+    [Fact]
+    public async void TestsWriteUser()
+    {
+        User user = new User() { Id = 1, Name = "Benjamin", IsActive = true };
+        _ = await UserDatabase.GetInstance().WriteUser(user);
+        Assert.True(System.IO.File.Exists("user.json"));
+    }
+
+    [Fact]
+    public async void TestReadUser()
+    {
+        User user = new User() { Id = 1, Name = "Juan", IsActive = true };
+        await UserDatabase.GetInstance().WriteUser(user);
+        var readUser = await UserDatabase.GetInstance().ReadUser("");
+        Assert.Equal("Juan", readUser.Name);
+    }
+}
