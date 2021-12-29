b44cf7312625e784072e2f63ad87e381fff12597
Benjamin Camacho <jahbenjah@hotmail.com>
2021-12-28 23:08:30 -0600

    Add .editorconfig

--- src/Singleton/Example/UserDatabase.cs
+++ src/Singleton/Example/UserDatabase.cs
@@ -6,7 +6,7 @@ public class UserDatabase : IUserDatabase
 {
     private static IUserDatabase _instance = new UserDatabase();
     public static IUserDatabase GetInstance() => _instance;
-    
+
     private UserDatabase()
     {
     }
@@ -19,7 +19,7 @@ public class UserDatabase : IUserDatabase
     }
     public async Task<bool> WriteUser(User user)
     {
-        await File.WriteAllTextAsync("user.json",JsonSerializer.Serialize(user));
+        await File.WriteAllTextAsync("user.json", JsonSerializer.Serialize(user));
         return true;
     }
 }
--- tests/Singleton.Tests/TestUserDatabase.cs
+++ tests/Singleton.Tests/TestUserDatabase.cs
@@ -7,7 +7,7 @@ public class TestUserDatabase
     [Fact]
     public async void TestsWriteUser()
     {
-        User user = new User() { Id = 1, Name = "Benjamin", IsActive = true };
+        User user = new() { Id = 1, Name = "Benjamin", IsActive = true };
         _ = await UserDatabase.GetInstance().WriteUser(user);
         Assert.True(System.IO.File.Exists("user.json"));
     }
@@ -15,9 +15,9 @@ public class TestUserDatabase
     [Fact]
     public async void TestReadUser()
     {
-        User user = new User() { Id = 1, Name = "Juan", IsActive = true };
+        User user = new() { Id = 1, Name = "Juan", IsActive = true };
         await UserDatabase.GetInstance().WriteUser(user);
-        var readUser = await UserDatabase.GetInstance().ReadUser("");
+        User readUser = await UserDatabase.GetInstance().ReadUser("");
         Assert.Equal("Juan", readUser.Name);
     }
 }
